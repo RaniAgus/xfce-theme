2337171209cd2b5b68ad6fd6b632f9797643e770
Agustin Ranieri <aguseranieri@gmail.com>
2022-04-11 15:34:28 -0300

    fixes varios

--- install.sh
+++ install.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -x
 
-# Configurar pares clave-valor
-configure() {
-  key=${1:?}
-  value=${2:?}
-  file=${3:?}
-  sed -i -e '/^'$key'=/ s/=.*/='$value'/' $file
-}
-
-download_config() {
-  file=${1:?}
-  wget -O "~/.config/$file" "https://raw.githubusercontent.com/RaniAgus/xfce-theme/main/$file"
-}
-
 # Display server
 sudo apt-get install -y xorg openbox
 
@@ -41,7 +28,7 @@ rm -rf Qogir-icon-theme
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Qogir-dark"
 
 # Fuentes
-sudo apt-get install fonts-roboto
+sudo apt-get install -y fonts-roboto
 xfconf-query -c xsettings -p /Gtk/FontName -s "Roboto 10"
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
@@ -52,14 +39,28 @@ sudo add-apt-repository ppa:gottcode/gcppa
 sudo apt-get update && sudo apt-get install -y \
   xfce4-whiskermenu-plugin
 
-configure "button-icon" "distributor-logo-xubuntu" "~/.config/xfce4/panel/whiskermenu-6.rc"
-
 # GTK config
 mkdir -pv ~/.config/gtk-3.0
-download_config "gtk-3.0/bookmarks"
-download_config "gtk-3.0/gtk.css"
+download() {
+  file=${1:?}
+  wget -O ~/.config/$file "https://raw.githubusercontent.com/RaniAgus/xfce-theme/main/$file"
+}
+download "gtk-3.0/bookmarks"
+download "gtk-3.0/gtk.css"
 
-# Perfil
+# Panel profile
 sudo apt-get install -y xfce4-panel-profiles
 wget https://github.com/RaniAgus/xfce-theme/raw/main/xfce4-panel-profile.tar.bz2
 xfce4-panel-profiles load ./xfce4-panel-profile.tar.bz2
+rm -v ./xfce4-panel-profile.tar.bz2
+
+# Whiskermenu config
+whiskermenu_set() {
+  key=${1:?}
+  value=${2:?}
+  sed -i -e '/^'$key'=/ s/=.*/='$value'/' ~/.config/xfce4/panel/whiskermenu-6.rc
+}
+whiskermenu_set "button-icon" "distributor-logo-xubuntu"
+
+# Logout
+xfce4-session-logout --logout
