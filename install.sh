b7b3c5cd36ffc59495e3ab07152db5caec81f77b
Agustin Ranieri <aguseranieri@gmail.com>
2022-04-11 12:10:00 -0300

    agrego configuracion de xfce

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y lightdm lightdm-gtk-greeter
 # Desktop environment
 sudo apt-get install -y xfce4 xfce4-panel-profiles
 
-# Temas
+# Tema
 sudo apt-get install -y git sassc
 git clone https://github.com/vinceliuice/Qogir-theme
 cd Qogir-theme
@@ -17,16 +17,22 @@ cd Qogir-theme
 ./install.sh --tweaks round
 cd ..
 rm -rf Qogir-theme
+xfconf-query -c xsettings -p /Net/ThemeName -s "Qogir-dark"
 
+# Icon pack
 git clone https://github.com/vinceliuice/Qogir-icon-theme
 cd Qogir-icon-theme
 ./install.sh
 cd ..
 rm -rf Qogir-icon-theme
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Qogir-dark"
 
 # Fuentes
 sudo apt-get install fonts-roboto
+xfconf-query -c xsettings -p /Gtk/FontName -s "Roboto 10"
+
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s "JetBrains Mono 10"
 
 # Plugins
 sudo add-apt-repository ppa:gottcode/gcppa
