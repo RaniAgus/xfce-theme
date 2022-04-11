a5f2df87d8bc57c811f9ebd988fed2cee7c9591d
Agustin Ranieri <aguseranieri@gmail.com>
2022-04-11 16:19:45 -0300

    muevo toda la instalacion a install.sh

--- configure.sh
+++ configure.sh
@@ -3,26 +3,13 @@
 set -e
 
 # Tema
-git clone https://github.com/vinceliuice/Qogir-theme
-cd Qogir-theme
-./install.sh
-./install.sh --tweaks round
-cd ..
-rm -rf Qogir-theme
 xfconf-query -c xsettings -p /Net/ThemeName -s "Qogir-dark"
 
 # Icon pack
-git clone https://github.com/vinceliuice/Qogir-icon-theme
-cd Qogir-icon-theme
-./install.sh
-cd ..
-rm -rf Qogir-icon-theme
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Qogir-dark"
 
 # Fuentes
 xfconf-query -c xsettings -p /Gtk/FontName -s "Roboto 10"
-
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
 xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s "JetBrains Mono 10"
 
 # GTK config
