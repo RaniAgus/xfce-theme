36bd9613cb4771b9255c3135e314eb6d8cc80fa0
Agustin Ranieri <aguseranieri@gmail.com>
2022-04-11 10:14:28 -0300

    first commit

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Display server
+sudo apt-get install -y xorg openbox
+
+# Display manager
+sudo apt-get install -y lightdm lightdm-gtk-greeter
+
+# Desktop environment
+sudo apt-get install -y xfce4 xfce4-panel-profiles
+
+# Temas
+sudo apt-get install -y git sassc
+git clone https://github.com/vinceliuice/Qogir-theme
+cd Qogir-theme
+./install.sh
+./install.sh --tweaks round
+cd ..
+rm -rf Qogir-theme
+
+git clone https://github.com/vinceliuice/Qogir-icon-theme
+cd Qogir-icon-theme-
+./install.sh
+cd ..
+rm -rf Qogir-icon-theme
+
+# Fuentes
+sudo apt-get install fonts-roboto
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
+# Plugins
+sudo add-apt-repository ppa:xubuntu-dev/extras
+sudo add-apt-repository ppa:gottcode/gcppa
+sudo apt-get update && sudo apt-get install -y \
+  xfce4-docklike-plugin \
+  xfce4-whiskermenu-plugin
+
+xfce4-panel -r
