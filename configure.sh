dc7d4cd211116c5e4e9a6d2fc804c46fc03e8f0b
Agustin Ranieri <aguseranieri@gmail.com>
2022-04-12 13:59:57 -0300

    agrego configuracion de whisker menu

--- configure.sh
+++ configure.sh
@@ -12,14 +12,19 @@ xfconf-query -c xsettings -p /Gtk/FontName -s "Roboto 10"
 xfconf-query -c xfwm4 -p /general/title_font -s "Roboto Bold 10"
 xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s "JetBrains Mono 10"
 
-# GTK config
-mkdir -pv ~/.config/gtk-3.0
 download() {
   file=${1:?}
   wget -O ~/.config/$file "https://raw.githubusercontent.com/RaniAgus/xfce-theme/main/$file"
 }
+
+# GTK config
+mkdir -pv ~/.config/gtk-3.0
 download "gtk-3.0/bookmarks"
 download "gtk-3.0/gtk.css"
+
+# Whisker Menu config
+mkdir -pv ~/.config/xfce4 ~/.config/xfce4/panel
+download "xfce4/panel/whiskermenu-6.rc"
 
 # Panel profile
 wget https://github.com/RaniAgus/xfce-theme/raw/main/xfce4-panel-profile.tar.bz2
