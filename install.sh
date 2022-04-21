a8c60b316b4fe32cbb78ef2fbce7953345c4e5f6
Agustin Ranieri <aguseranieri@gmail.com>
2022-04-21 16:30:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ sudo apt install --reinstall ca-certificates
 
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:gottcode/gcppa
-sudo add-apt-repository -y ppa:numix/ppa
 sudo apt-get update && sudo apt-get install -y \
   xorg \
   lightdm \
@@ -14,7 +13,6 @@ sudo apt-get update && sudo apt-get install -y \
   xfce4 \
   git-all \
   sassc \
-  numix-blue-gtk-theme \
   fonts-roboto \
   xfce4-whiskermenu-plugin \
   xfce4-panel-profiles
@@ -32,9 +30,3 @@ cd Qogir-icon-theme
 ./install.sh
 cd ..
 rm -rf Qogir-icon-theme
-
-# https://askubuntu.com/a/500813
-sudo mkdir /usr/share/xsessions/hidden
-sudo dpkg-divert --rename \
-      --divert /usr/share/xsessions/hidden/ubuntu.desktop \
-      --add /usr/share/xsessions/ubuntu.desktop
